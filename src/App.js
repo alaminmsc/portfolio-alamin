d76ba2c9d64c730dd1e450f549f3093e7971c267
alaminiium2019 <alamin.iium2017@gmail.com>
2021-05-05 17:56:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Grid } from "@material-ui/core";
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Profile from "./Components/Profile/Profile";
+import Resume from "./Pages/Resume/Resume";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid container>
+        <Grid item xs={12} md={4} lg={3} style={{ background: "yellow" }}>
+          <Profile></Profile>
+        </Grid>
+        <Grid item xs style={{ background: "red" }}>
+          <Header></Header>
+          <Router>
+            <Switch>
+              <Route path="/portfolio">
+                <Portfolio></Portfolio>
+              </Route>
+              <Route path="/">
+                <Resume></Resume>
+              </Route>
+            </Switch>
+          </Router>
+          <Footer></Footer>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/Pages/Resume/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div>
+            <h3>This is Resume section</h3>
+        </div>
+    );
+};
+
+export default Resume;
